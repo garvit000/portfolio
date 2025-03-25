16d9dd158e42bb61db1c06b1a8e238dcfa4481a7
Garvit S Rathore <garvit.sr7@gmail.com>
2025-03-25 22:19:00 +0530

    Portfolio init

--- README.md
+++
@@ -1,3 +0,0 @@
-# On Pause
-## Portfolio
-My public portfolio displaying my achievements and projects I've made alongside my journey.
---
+++ client/src/Components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Footer() {
+  return (
+    <>
+    <footer className="bg-gray-800 text-white p-4 w-screen h-fit bottom-0">
+      &copy; GSR - Projects 2025
+      </footer>
+    </>
+  )
+}
+export default Footer;
