57b484acfd24897fcee54172c64945c7d98df0c1
Garvit S Rathore <garvit.sr7@gmail.com>
2025-03-30 01:58:45 +0530

    Fixed overflow issue for smaller devices

--- client/src/Components/footer.tsx
+++ client/src/Components/footer.tsx
@@ -3,13 +3,13 @@ import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
     <div className="bg-gray-800 mx-auto text-white w-screen h-screen flex flex-col pt-22">
-     <div className="flex-grow">
-        <h1 className="text-9xl font-bold font-mono text-cyan-300 ml-20 mt-10">
+      <div className="flex-grow flex flex-col">
+        <h1 className="text-4xl md:text-6xl lg:text-9xl text-center md:text-left font-bold font-mono text-cyan-300 md:ml-20 md:mt-10">
           Thanks for <br/> visiting!
         </h1>
 
-        <div className="flex-wrap justify-self-end sm:w-sm w-fit -mt-20 mr-20 rounded-2xl p-5 bg-gradient-to-r from-purple-400 to-emerald-500">
-          <p className="font-bold text-3xl text-center">Connect:</p>
+        <div className="mx-auto md:mr-20 md:self-end md:-mt-20 mt-8 rounded-2xl p-5 bg-gradient-to-r from-purple-400 to-emerald-500 max-w-xs">
+          <p className="font-bold text-3xl">Connect:</p>
           <div className="flex justify-between pt-3 cursor-pointer">
             <a href="https://github.com/garvit000" target="_blank">
               <FaGithub className="text-3xl text-gray-300 hover:text-4xl transition-all duration-300" />
