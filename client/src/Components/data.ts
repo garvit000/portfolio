11aa531ba76cad70d80a7c4960738e3b2318808c
Garvit Singh Rathore <garvit.sr7@gmail.com>
2026-02-28 02:33:57 +0530

    Update Projects data

--- client/src/Components/data.ts
+++ client/src/Components/data.ts
@@ -4,18 +4,21 @@ const Data = [
         description: "This was a first semester project website for our french language, in which we will show you all the famous architectures from french regions which are known for their stunning design and architecure.",
         link: "https://github.com/garvit000/french-architectures",
         imageUrl: "https://i.ibb.co/27qwY3dK/Screenshot-2025-03-31-220157.png",
+        liveUrl: "https://garvit000.github.io/french-architectures/",
     },
     {
-        title: "Password Generator",
-        description: "A simple password generator that creates secure passwords.",
-        link: "https://github.com/garvit000/PasswordGenerator",
-        imageUrl: "https://i.ibb.co/bj5JMYgw/Screenshot-2025-03-31-225352.png",
+        title: "JanPath",
+        description: "JanPath is a Civic Grievance Intelligence Platform, streamlining the reporting and resolution of public grievances.",
+        link: "https://github.com/garvit000/JanPath",
+        imageUrl: "https://i.ibb.co/DHyhCFFk/image.png",
+        liveUrl: "https://janpath.vercel.app",
     },
     {
-        title: "QR-Code Generator",
-        description: "A simple QR_code generator that can create qr codes for any link, text, image etc.",
-        link: "https://github.com/garvit000/qr-code-generator",
-        imageUrl: "https://github.com/garvit000/qr-code-generator/blob/master/public/qr-logo.png?raw=true",
+        title: "Prognosis Care",
+        description: "Prognosis Care is a comprehensive healthcare platform designed to connect patients with doctors, manage appointments, and provide seamless medical services.",
+        link: "https://github.com/garvit000/prognosis-care",
+        imageUrl: "https://i.ibb.co/QvngpJwH/image.png",
+        liveUrl: "https://prognosis-care.vercel.app",
     }
 ];
 export default Data;
