3638dec165b9ebe2fa99a16ada7d661cfcf7ec0a
Garvit Singh Rathore <garvit.sr7@gmail.com>
2026-04-01 23:17:21 +0530

    Track web analytics using vercel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import Main from './Components/main'
 import Projects from './Components/projects'
 import About from "./Components/about"
 import Skills from './Components/skills'
+import { Analytics } from "@vercel/analytics/next"
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Analytics />
       <Navbar/>
       <Main/>
       <About/>
